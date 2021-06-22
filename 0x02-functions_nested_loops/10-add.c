c5fcd0252f8fba249bffecbf7d49b63329189c6e
LhetMagnan <lmagnan17@alustudent.com>
2021-06-22 18:02:00 +0000

    Files added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - adding two integers and returns the result.
+ * @a: number a
+ * @b: number b
+ * result: sum of a and b
+ * Return: return the sum of a and b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: Number to be checked
+ * Return: return the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (n % 10) * -1;
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
