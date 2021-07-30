c846e03af65be57c8ccb558ddbd32131414e5a44
LhetMagnan <lmagnan17@alustudent.com>
2021-07-30 01:37:05 +0000

    file updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,15 +9,31 @@
  * Return: address of the new element
  */
 
-list_t *add_node(list_t **head, const int n)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_node;
+	int len_str;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = (list_t *) malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	for (len_str = 0; str[len_str] != '\0';  len_str++)
+	;
+
+	new_node->len = len_str;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
