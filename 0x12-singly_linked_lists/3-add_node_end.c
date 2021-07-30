e5bd66787a949be48660dc4eaa933b4d1634b082
LhetMagnan <lmagnan17@alustudent.com>
2021-07-30 01:43:00 +0000

    file updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,28 +10,37 @@
  * Return: the address of the new element, or NULL if it failed
  */
 
-list_t *add_node_end(list_t **head, const int n)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
-	list_t *tmp;
+	list_t *current;
+	list_t *new_node;
+	int c;
 
-	newNode = malloc(sizeof(list_t));
-	if (newNode == NULL)
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
-	newNode->n = n;
-	newNode->next = NULL;
-	if (*head == NULL)
-	{
-		*head = newNode;
 	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = c;
+	new_node->next = NULL;
+
+	if (current)
+		current->next = new_node;
 	else
-	{
-		tmp = *head;
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-		tmp->next = newNode;
-	}
-	return (newNode);
+		*head = new_node;
+	return (new_node);
 }
