ecb15832871ef1c9f7e663fbf891880af53b0b0c
LhetMagnan <lmagnan17@alustudent.com>
2021-07-30 01:41:06 +0000

    file updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t
+ * add_node_end - Adds a new node at the end of a list_t
  * @head: input header pointer
  * @n: Input data int
- * Description: function that adds a new node at the end of a listint_t
+ * Description: function that adds a new node at the end of a list_t
  * Return: the address of the new element, or NULL if it failed
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+list_t *add_node_end(list_t **head, const int n)
 {
-	listint_t *newNode;
-	listint_t *tmp;
+	list_t *newNode;
+	list_t *tmp;
 
-	newNode = malloc(sizeof(listint_t));
+	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
