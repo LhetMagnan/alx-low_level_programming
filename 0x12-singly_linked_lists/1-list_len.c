f9da21b68e5b9607fde0f4343c980a931c7f4a2d
LhetMagnan <lmagnan17@alustudent.com>
2021-07-30 01:33:37 +0000

    file updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list.
+ * list_len - Returns the number of elements in a linked list_t list.
  * @h: header pointer
  * Description: function that returns the number of elements in a linked
  * Return: number of elements
  */
 
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t cont = 0;
 
