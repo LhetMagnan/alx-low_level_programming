a9c5fc35030595b069b943266b965c61b9fec3c9
LhetMagnan <lmagnan17@alustudent.com>
2021-07-30 01:25:22 +0000

    file updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list
+ * print_list - Prints all the elements of a list_t list
  * @h: header pointer
- * Description: function that prints all the elements of a listint_t list
+ * Description: function that prints all the elements of a list_t list
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t cont = 0;
 
