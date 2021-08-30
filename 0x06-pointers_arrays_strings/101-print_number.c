7e4b039f3ffa361c72cccc4032215560294324bc
LhetMagnan <lmagnan17@alustudent.com>
2021-08-30 23:45:26 +0000

    print_number file updated

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * print_number - Prints an Integer
+  * @n: Integer number to be printed
+  * Return: 0
+ **/
+
+void print_number(int n)
+{
+	unsigned int number;
+
+	number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -n;
+	}
+
+	if (number / 10 != 0)
+	{
+		print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
+}
